ab88eeaa2ec68ec5ff54f312b0964b76e48e56a2
boris_mikhaylov <boris_mikhaylov@bc0edfbe-c936-4687-b64d-f70bc3985e72>
2009-03-30 05:46:36 +0000

    inlined cross_feed() calls.
    git-svn-id: svn://svn.code.sf.net/p/bs2b/code/trunk/libbs2b@87 bc0edfbe-c936-4687-b64d-f70bc3985e72

--- src/bs2bclass.h
+++ src/bs2bclass.h
@@ -45,38 +45,135 @@ public:
 	char const *runtime_version( void );
 	uint32_t    runtime_version_int( void );
 
-	void cross_feed( double        *sample, int n = 1 );
-	void cross_feed( float         *sample, int n = 1 );
-	void cross_feed( int32_t       *sample, int n = 1 );
-	void cross_feed( uint32_t      *sample, int n = 1 );
-	void cross_feed( int16_t       *sample, int n = 1 );
-	void cross_feed( uint16_t      *sample, int n = 1 );
-	void cross_feed( int8_t        *sample, int n = 1 );
-	void cross_feed( uint8_t       *sample, int n = 1 );
-	void cross_feed( bs2b_int24_t  *sample, int n = 1 );
-	void cross_feed( bs2b_uint24_t *sample, int n = 1 );
+	inline void cross_feed( double *sample, int n = 1 )
+	{
+		bs2b_cross_feed_d( bs2bdp, sample, n );
+	}
 
-	void cross_feed_be( double        *sample, int n = 1 );
-	void cross_feed_be( float         *sample, int n = 1 );
-	void cross_feed_be( int32_t       *sample, int n = 1 );
-	void cross_feed_be( uint32_t      *sample, int n = 1 );
-	void cross_feed_be( int16_t       *sample, int n = 1 );
-	void cross_feed_be( uint16_t      *sample, int n = 1 );
-	void cross_feed_be( int8_t        *sample, int n = 1 );
-	void cross_feed_be( uint8_t       *sample, int n = 1 );
-	void cross_feed_be( bs2b_int24_t  *sample, int n = 1 );
-	void cross_feed_be( bs2b_uint24_t *sample, int n = 1 );
+	inline void cross_feed_be( double *sample, int n = 1 )
+	{
+		bs2b_cross_feed_dbe( bs2bdp, sample, n );
+	}
 
-	void cross_feed_le( double        *sample, int n = 1 );
-	void cross_feed_le( float         *sample, int n = 1 );
-	void cross_feed_le( int32_t       *sample, int n = 1 );
-	void cross_feed_le( uint32_t      *sample, int n = 1 );
-	void cross_feed_le( int16_t       *sample, int n = 1 );
-	void cross_feed_le( uint16_t      *sample, int n = 1 );
-	void cross_feed_le( int8_t        *sample, int n = 1 );
-	void cross_feed_le( uint8_t       *sample, int n = 1 );
-	void cross_feed_le( bs2b_int24_t  *sample, int n = 1 );
-	void cross_feed_le( bs2b_uint24_t *sample, int n = 1 );
+	inline void cross_feed_le( double *sample, int n = 1 )
+	{
+		bs2b_cross_feed_dle( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( float *sample, int n = 1 )
+	{
+		bs2b_cross_feed_f( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( float *sample, int n = 1 )
+	{
+		bs2b_cross_feed_fbe( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( float *sample, int n = 1 )
+	{
+		bs2b_cross_feed_fle( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( int32_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s32( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( uint32_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u32( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( int32_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s32be( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( uint32_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u32be( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( int32_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s32le( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( uint32_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u32le( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( int16_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s16( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( uint16_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u16( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( int16_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s16be( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( uint16_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u16be( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( int16_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s16le( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( uint16_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u16le( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( int8_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s8( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( uint8_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u8( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( bs2b_int24_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s24( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed( bs2b_uint24_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u24( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( bs2b_int24_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s24be( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_be( bs2b_uint24_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u24be( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( bs2b_int24_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_s24le( bs2bdp, sample, n );
+	}
+
+	inline void cross_feed_le( bs2b_uint24_t *sample, int n = 1 )
+	{
+		bs2b_cross_feed_u24le( bs2bdp, sample, n );
+	}
 }; // class bs2b_base
 
 #endif // BS2BCLASS_H
